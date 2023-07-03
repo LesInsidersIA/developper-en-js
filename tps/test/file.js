c961918cafa80397616f7fe591c22102ef8a6481
yosagaf <sagafysf@gmail.com>
2023-07-03 16:26:44 +0200

    Après Jour 1 de formation

---
+++ tps/test/file.js
@@ -0,0 +1,8 @@
+let age = 16;
+
+if (age >= 18){
+    console.log("Tu es majeur")
+}
+else{
+    console.log("Tu es mineur")
+}
---
+++ tps/test/js/app.js
@@ -0,0 +1,9 @@
+
+let age = 16;
+
+if (age >= 18){
+    console.log("Tu es majeur")
+}
+else{
+    console.log("Tu es mineur")
+}
