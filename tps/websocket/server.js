27c3833e9dd6ddddab29e047c94591832b13204a
yosagaf <sagafysf@gmail.com>
2023-07-04 20:49:20 +0200

    Nettoyage done

--- tps/websocket/server.js
+++ tps/websocket/server.js
@@ -2,6 +2,7 @@
 
 const WebSocket = require('ws');
 
+// Crée une nouvelle instance du serveur WebSocket en écoutant le port 8080
 const wss = new WebSocket.Server({ port: 8080 });
 
 // Événement déclenché lorsqu'un client se connecte
@@ -9,6 +10,7 @@ wss.on('connection', function connection(ws) {
   // Événement déclenché lorsqu'un message est reçu du client
   ws.on('message', function incoming(message) {
     console.log('Reçu du client :', message);
+    // Envoie d'une réponse au client
     ws.send('Réponse du serveur : ' + message);
   });
 });
